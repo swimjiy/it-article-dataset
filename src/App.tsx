e5e1379cb3619320c61ab5fb3a5734b7c0fc2d0b
swimjiy <swimjiy@gmail.com>
2020-12-16 23:27:19 +0900

    Apply antd to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Button } from 'antd';
+
+import './App.css';
+import Table from './Table';
 
 const App: React.FC = () => (
   <div>
     Hello, World!
+    <Button type="primary">Button</Button>
+    <Table />
   </div>
 );
 export default App;
