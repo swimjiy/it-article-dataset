288cd31672bfdcaa3a058a7191180c8296b258ff
swimjiy <swimjiy@gmail.com>
2020-12-18 22:57:15 +0900

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Typography } from 'antd';
+
+const { Title, Text } = Typography;
 
 import './App.css';
 import Table from './Table';
 
 const App: React.FC = () => (
   <div>
-    Hello, World!
+    <Title>기술 아티클 모음</Title>
+    <Text type="secondary">내가 읽으려고 만든 기술 아티클 테이블</Text>
     <Button type="primary">Button</Button>
     <Table />
   </div>
