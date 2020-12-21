29828335ef0be5f1be303ccc8d77407cb72d4cc2
swimjiy <swimjiy@gmail.com>
2020-12-21 23:20:39 +0900

    Add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Button, Typography } from 'antd';
+import { Button, Layout, Typography } from 'antd';
 
 const { Title, Text } = Typography;
 
 import './App.css';
 import Table from './Table';
 
-const App: React.FC = () => (
-  <div>
-    <Title>기술 아티클 모음</Title>
-    <Text type="secondary">내가 읽으려고 만든 기술 아티클 테이블</Text>
-    <Button type="primary">Button</Button>
-    <Table />
-  </div>
-);
+const App: React.FC = () => {
+  const { Header, Footer, Content } = Layout;
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        <Title>기술 아티클 모음</Title>
+        <Text type="secondary">내가 읽으려고 만든 기술 아티클 테이블</Text>
+        <Button type="primary">Button</Button>
+        <Table />
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+};
 export default App;
