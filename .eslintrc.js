66d482890b13007c9c2fdb912d9b6ad85968441f
swimjiy <swimjiy@gmail.com>
2020-12-23 23:51:51 +0900

    chore: update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  extends: ['airbnb-typescript'],
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parserOptions: {
     ecmaVersion: 2015,
     sourceType: 'module',
@@ -14,6 +17,27 @@ module.exports = {
   ],
   rules: {
     'react/prop-types': 'off',
+    'max-len': ['error', {
+      code: 120,
+      tabWidth: 2,
+      ignoreComments: true,
+    }],
+    'no-console': ['error', {
+      allow: ['error', 'info'],
+    }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+    }],
+    '@typescript-eslint/no-unsafe-assignment': 1,
+    '@typescript-eslint/no-floating-promises': 1,
   },
   settings: {
     'import/resolver': {
